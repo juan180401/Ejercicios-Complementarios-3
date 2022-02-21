9d71a6fb147aee4633db3479ca6cd89c5a2e6f23
juan180401 <jachpera2015@gmail.com>
2022-02-20 22:40:59 -0600

    ejercicios compementarios completos

---
+++ Ejercicio5/app/src/main/java/sv/edu/udb/dsm/guia3/ejemplo2/ejemplo_grid_layout.java
@@ -0,0 +1,15 @@
+package sv.edu.udb.dsm.guia3.ejemplo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ejemplo_grid_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplo_grid_layout);
+    }
+
+}
